fa49be72049ede3e6d11401e9bdbc57b2824eb3c
Daniel Silva de Souza <danielss2990@gmail.com>
2020-07-13 16:31:24 -0300

    Projeto criado

---
+++ src/test/java/com/daniel/modelagem/ModelagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.modelagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
