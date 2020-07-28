2493f26edf60e7a1d61694ea17cfe869a8158092
Daniel Silva de Souza <danielss2990@gmail.com>
2020-07-28 17:18:20 -0300

    Estado e cidade

--- src/main/java/com/daniel/modelagem/ModelagemApplication.java
+++ src/main/java/com/daniel/modelagem/ModelagemApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.daniel.modelagem.domain.Categoria;
+import com.daniel.modelagem.domain.Cidade;
+import com.daniel.modelagem.domain.Estado;
 import com.daniel.modelagem.domain.Produto;
 import com.daniel.modelagem.repositories.CategoriaRepository;
+import com.daniel.modelagem.repositories.CidadeRepository;
+import com.daniel.modelagem.repositories.EstadoRepository;
 import com.daniel.modelagem.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,14 @@ public class ModelagemApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemApplication.class, args);
@@ -43,9 +55,24 @@ public class ModelagemApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2 = new Estado(null,"São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia",est1);
+		Cidade c2 = new Cidade(null,"São Paulo",est2);
+		Cidade c3 = new Cidade(null,"Campinas",est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
 	}
 	
---
+++ src/main/java/com/daniel/modelagem/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.daniel.modelagem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.daniel.modelagem.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+	
+}
